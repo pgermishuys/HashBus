d1be43e374f40e43efbb083964f259a46b4181c2
Adam Ralph <adam@adamralph.com>
2015-10-31 16:54:03 +0100

    added mutex locking to FileListRepository

--- src/HashBus.Projection.MentionLeaderboard/FileListRepository.cs
+++ src/HashBus.Projection.MentionLeaderboard/FileListRepository.cs
@@ -1,18 +1,22 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 
 namespace HashBus.Projection
 {
-    public class FileListRepository<TValue> : IRepository<string, IEnumerable<TValue>>
+    public sealed class FileListRepository<TValue> : IRepository<string, IEnumerable<TValue>>, IDisposable
     {
         private readonly string folderName;
+        private readonly Mutex @lock;
 
         public FileListRepository(string folderName)
         {
             this.folderName = folderName;
+            this.@lock = new Mutex(false, folderName.Replace('\\', '/'));
         }
 
         public Task<IEnumerable<TValue>> GetAsync(string key)
@@ -23,22 +27,57 @@ namespace HashBus.Projection
                 return Task.FromResult(Enumerable.Empty<TValue>());
             }
 
-            using (var textReader = new StreamReader(fileName))
+            try
             {
-                return Task.FromResult(
-                    new JsonSerializer().Deserialize<IEnumerable<TValue>>(new JsonTextReader(textReader)) ?? Enumerable.Empty<TValue>());
+                this.@lock.WaitOne();
+            }
+            catch (AbandonedMutexException)
+            {
+            }
+
+            try
+            {
+                using (var textReader = new StreamReader(fileName))
+                {
+                    return Task.FromResult(
+                        new JsonSerializer().Deserialize<IEnumerable<TValue>>(new JsonTextReader(textReader)) ?? Enumerable.Empty<TValue>());
+                }
+            }
+            finally
+            {
+                this.@lock.ReleaseMutex();
             }
         }
 
         public Task SaveAsync(string key, IEnumerable<TValue> value)
         {
             var fileName = Path.Combine(this.folderName, $"{key}.json");
-            using (var textReader = new StreamWriter(fileName))
+            try
             {
-                new JsonSerializer().Serialize(new JsonTextWriter(textReader), value);
+                this.@lock.WaitOne();
+            }
+            catch (AbandonedMutexException)
+            {
+            }
+
+            try
+            {
+                using (var textReader = new StreamWriter(fileName))
+                {
+                    new JsonSerializer().Serialize(new JsonTextWriter(textReader), value);
+                }
+            }
+            finally
+            {
+                this.@lock.ReleaseMutex();
             }
 
             return Task.FromResult(0);
+        }
+
+        public void Dispose()
+        {
+            this.@lock.Dispose();
         }
     }
 }
